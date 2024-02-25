1b9c7f090f0f7252d748e163aa322fb01d549ad5
Denis K <wolf-den@tut.by>
2024-02-25 19:25:30 +0300

    feat: add loader

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { useState, useEffect, Alert } from 'react';
-import { View, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, FlatList, Alert, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 import Post from './components/Post';
 import { randomDate, formatDate } from './utils/common';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [items, setItems] = useState();
 
-  async function fetchData() {
+  const fetchData = async () => {
     try {
+      setIsLoading(true);
       const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts');
       const { data: photos } = await axios.get('http://jsonplaceholder.typicode.com/photos?_start=0&_limit=100');
 
@@ -27,12 +29,27 @@ export default function App() {
     } catch (err) {
       console.error(err);
       Alert.alert('Error', 'Oops, failed to retrieve data!');
+    } finally {
+      setIsLoading(false);
     }
   }
 
   useEffect(() => {
     fetchData();
   }, [])
+
+  if (isLoading) {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <ActivityIndicator size="large" />
+        <Text style={{marginTop: 15}}>Loading...</Text>
+      </View>
+    )
+  }
 
   return (
     <View>
