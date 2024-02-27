68b7d321ef45249777f769ba13653cc24409f055
Denis K <wolf-den@tut.by>
2024-02-27 19:03:39 +0300

    feat: apk install

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
-import EmptySpace from './components/EmptySpace';
 import Navigation from './screens/Navigation';
 
 export default function App() {
   return (
     <>
-      <EmptySpace />
       <Navigation />
     </>
   );
